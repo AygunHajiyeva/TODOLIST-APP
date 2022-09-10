1e0ce092ee3e87cf4ab57650f7da018b0b133e7b
AygunHajiyeva <aygunhajiyevaa@gmail.com>
2022-09-10 16:57:15 +0400

    added checkbox functionality

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,19 +2,31 @@ import React from "react";
 
 export default function({ todoList, setTodoList }) {
   const handleDelete = (selectedTodo) => {
-    setTodoList((todoList) =>
-      todoList.filter((item) => item.id !== selectedTodo)
+    setTodoList((prevTodoList) =>
+      prevTodoList.filter((item) => item.id !== selectedTodo)
     );
   };
+
+  const handleIsComplete = (id) => {
+    setTodoList((prevTodoList) => prevTodoList.map((item) => item.id === id ? {...item, isComplete:!item.isComplete} :item));
+    
+  };
+  
 
   return (
     <div className="todolist">
       {todoList.map((todo) => (
-        <div key={todo.id} className="todo">
+        <div key={todo.id} className="todo" style={todo.isComplete ? {textDecorationLine: 'line-through'}:{}}>
           {todo.todo}
           <button onClick={() => handleDelete(todo.id)}>
             <i class="fa-solid fa-trash"></i>
           </button>
+          <input
+            type="checkbox"
+            value={todo.isComplete}
+            onChange={() => handleIsComplete(todo.id)}
+            
+          />
         </div>
       ))}
     </div>
